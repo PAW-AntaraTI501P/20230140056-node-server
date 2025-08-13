e7ec12add89c1a8c16a6cf0b946c7854afae291c
Dababah <pawasalee96@gamil.com>
2025-08-13 15:55:38 +0700

    connect database dan view data

---
+++ TUGAS/tugas6.md
@@ -0,0 +1,2 @@
+tampilan todo-view  ![alt text](image-4.png)
+tampilan phpmyadmin ![alt text](image-5.png)
--- app.js
+++ app.js
@@ -1,36 +1,67 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3001;
-const { router: todoRoutes, todos } = require("./routes/todo.js");
+const db = require("./database/db"); // Koneksi database
+const todoRoutes = require("./routes/tododb.js"); // API routes
 
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.set("view engine", "ejs");
 
+// API CRUD
 app.use("/todos", todoRoutes);
 
+// API JSON: ambil data todos
 app.get("/todos-data", (req, res) => {
-  res.json(todos);
+  db.query("SELECT * FROM todos", (err, results) => {
+    if (err) return res.status(500).send("Internal Server Error");
+    res.json(results);
+  });
 });
 
+// Page EJS: daftar todos
+app.get("/todos-pages", (req, res) => {
+  db.query("SELECT * FROM todos", (err, results) => {
+    if (err) return res.status(500).send("Internal Server Error");
+    res.render("todo", { todos: results });
+  });
+});
+
+// Page EJS: daftar todos lain
 app.get("/todos-list", (req, res) => {
-  res.render("todos-page", { todos: todos });
+  db.query("SELECT * FROM todos", (err, results) => {
+    if (err) return res.status(500).send("Internal Server Error");
+    res.render("todos-page", { todos: results });
+  });
 });
 
+// Halaman index
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+// Halaman contact
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
-
+app.get("/todo-view", (req, res) => {
+  db.query("SELECT * FROM todos", (err, todos) => {
+    if (err) return res.status(500).send("Internal Server Error");
+    res.render("todo", {
+      todos: todos,
+    });
+  });
+});
+// 404 Handler
 app.use((req, res) => {
   res.status(404).send("404 - Page Not Found");
 });
 
+// Jalankan server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`✅ Server running on http://localhost:${port}`);
 });
