e133fb75f770f0cc3512895d200e6a26ce5133cb
Dababah <pawasalee96@gamil.com>
2025-08-17 23:52:58 +0700

    template views

---
+++ TUGAS/tugas7.md
@@ -0,0 +1,3 @@
+INDEX ![alt text](image-10.png)
+CONTACT ![alt text](image-9.png)
+TODOS LIST![alt text](image-8.png)
--- app.js
+++ app.js
@@ -1,67 +1,58 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const db = require("./database/db"); // Koneksi database
-const todoRoutes = require("./routes/tododb.js"); // API routes
-
 const app = express();
-const port = process.env.PORT || 3001;
+const todoRoutes = require("./routes/tododb.js");
+const cors = require("cors");
+const port = process.env.PORT;
+const { todos } = require("./routes/todo.js");
+const db = require("./database/db");
 
-// Middleware
+const expressLayouts = require("express-ejs-layouts");
+app.use(expressLayouts);
+
 app.use(cors());
 app.use(express.json());
 app.set("view engine", "ejs");
 
-// API CRUD
 app.use("/todos", todoRoutes);
 
-// API JSON: ambil data todos
 app.get("/todos-data", (req, res) => {
-  db.query("SELECT * FROM todos", (err, results) => {
-    if (err) return res.status(500).send("Internal Server Error");
-    res.json(results);
-  });
+  res.json(todos);
 });
 
-// Page EJS: daftar todos
-app.get("/todos-pages", (req, res) => {
-  db.query("SELECT * FROM todos", (err, results) => {
-    if (err) return res.status(500).send("Internal Server Error");
-    res.render("todo", { todos: results });
-  });
-});
-
-// Page EJS: daftar todos lain
 app.get("/todos-list", (req, res) => {
-  db.query("SELECT * FROM todos", (err, results) => {
-    if (err) return res.status(500).send("Internal Server Error");
-    res.render("todos-page", { todos: results });
+  res.render("todos-page", { 
+    todos: todos,
+    layout: "layouts/main-layout",
   });
 });
 
-// Halaman index
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    layout: "layouts/main-layout",
+  });
 });
 
-// Halaman contact
 app.get("/contact", (req, res) => {
-  res.render("contact");
+  res.render("contact", {
+    layout: "layouts/main-layout",
+  });
 });
+
 app.get("/todo-view", (req, res) => {
   db.query("SELECT * FROM todos", (err, todos) => {
     if (err) return res.status(500).send("Internal Server Error");
     res.render("todo", {
       todos: todos,
+      layout: "layouts/main-layout",
     });
   });
 });
-// 404 Handler
+
 app.use((req, res) => {
   res.status(404).send("404 - Page Not Found");
 });
 
-// Jalankan server
 app.listen(port, () => {
-  console.log(`✅ Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
